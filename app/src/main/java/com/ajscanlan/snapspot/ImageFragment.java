965566199e41f5523531efdece42ec1a3aa56f70
Alexander Scanlan <ajscanlan27@gmail.com>
2015-10-06 14:58:20 +0100

    Added bitmaps, markers, image fragment, onclickmarkerlistener

---
+++ app/src/main/java/com/ajscanlan/snapspot/ImageFragment.java
@@ -0,0 +1,136 @@
+package com.ajscanlan.snapspot;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ImageFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ImageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ImageFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String id;
+    private static ImageView imageView;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ImageFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ImageFragment newInstance(String param1, String param2, String id) {
+        ImageFragment fragment = new ImageFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+
+        fragment.id = id;
+
+        Log.d("HASHMAP", "IN FRAGMENT " + fragment.id);
+        Log.d("HASHMAP", "IN FRAGMENT " + MapsActivity.hashMap.get(fragment.id));
+
+        if(imageView == null) Log.d("HASHMAP", "IN FRAGMENT, IMAGEVIEW NULL");
+
+        return fragment;
+    }
+
+    public ImageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_image, container, false);
+        imageView = (ImageView) v.findViewById(R.id.imageView);
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (OnFragmentInteractionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        Log.d("HASHMAP", "IN START" + id);
+        Log.d("HASHMAP", "IN START" + MapsActivity.hashMap.get(id));
+
+        if(imageView == null) Log.d("HASHMAP", "IN FRAGMENT, IMAGEVIEW NULL");
+
+//        Log.d("HASHMAP", "IN FRAGMENT" + MapsActivity.hashMap.get(id).toString());
+        imageView.setImageResource(MapsActivity.hashMap.get(id));
+    }
+}
--- app/src/main/java/com/ajscanlan/snapspot/MapsActivity.java
+++ app/src/main/java/com/ajscanlan/snapspot/MapsActivity.java
@@ -1,23 +1,39 @@
 package com.ajscanlan.snapspot;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.HashMap;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, ImageFragment.OnFragmentInteractionListener{
 
     private GoogleMap mMap;
+    static HashMap<String, Integer> hashMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -37,10 +53,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
+
+        //Convert resource to bitmap
+        Bitmap bmp1 = resourceToBitmap(R.drawable.test2);
+        Bitmap bmp2 = resourceToBitmap(R.drawable.test3);
+        Bitmap bmp3 = resourceToBitmap(R.drawable.test4);
+        Bitmap bmp4 = resourceToBitmap(R.drawable.test5);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng paris = new LatLng(48, 2.3);
+        LatLng newYork = new LatLng(40, -73);
+        LatLng brazil = new LatLng(-15, -47);
+
+        Marker marker1 = mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+                .icon(BitmapDescriptorFactory.fromBitmap(bmp1)));
+
+        hashMap.put(marker1.getId(), R.drawable.test2);
+        //Log.d("HASHMAP", hashMap.get(marker1.getId()).toString());
+
+        Marker marker2 = mMap.addMarker(new MarkerOptions()
+                .position(paris)
+                .title("Marker in Paris")
+                .icon(BitmapDescriptorFactory.fromBitmap(bmp2)));
+        hashMap.put(marker2.getId(), R.drawable.test3);
+
+        Marker marker3 = mMap.addMarker(new MarkerOptions()
+                .position(newYork)
+                .title("Marker in New York")
+                .icon(BitmapDescriptorFactory.fromBitmap(bmp3)));
+        hashMap.put(marker3.getId(), R.drawable.test4);
+
+        Marker marker4 = mMap.addMarker(new MarkerOptions()
+                .position(brazil)
+                .title("Marker in Brazil")
+                .icon(BitmapDescriptorFactory.fromBitmap(bmp4)));
+        hashMap.put(marker4.getId(), R.drawable.test5);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        Log.d("HASHMAP", "m0 " + marker1.getId());
+        Log.d("HASHMAP", "2130837640 " + hashMap.get(marker1.getId()));
+
+    }
+
+    private Bitmap resourceToBitmap(int resource){
+        //Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), resource),200,200,true);
+        Canvas canvas1 = new Canvas(bmp);
+
+        // paint defines the text color,
+        // stroke width, size
+        Paint color = new Paint();
+        color.setTextSize(35);
+        color.setColor(Color.BLACK);
+
+        //modify canvas
+        canvas1.drawBitmap(bmp, 0, 0, color);
+        //canvas1.drawText("User Name!", 30, 40, color);
+
+        return bmp;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        Log.d("HASHMAP", "in method " + marker.getId());
+        Log.d("HASHMAP", "in method " + hashMap.get(marker.getId()));
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.map, ImageFragment.newInstance(null, null, marker.getId()))
+                .addToBackStack("")
+                .commit();
+
+        return false;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
